80b7a0751d407079553e8c5f6d36d98a6778f5fb
aapani-dukan <shivraj8404@gmail.com>
2025-05-20 15:30:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Login from './pages/Login';
 import SellerDashboard from './pages/SellerDashboard';
 import CustomerDashboard from './pages/CustomerDashboard';
 import WelcomeMenu from './components/WelcomeMenu'; // Adjusted path
-
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard"; // यह पेज आप पहले से बना चुके हैं
 function App() {
   const [user, setUser] = useState(null);
   const [products, setProducts] = useState([]);
-
+<Route path="/admin8404-login" element={<AdminLogin />} />
+<Route path="/admin-dashboard" element={<AdminDashboard />} />
+  
   if (!user) {
     return (
       <div className="app">
