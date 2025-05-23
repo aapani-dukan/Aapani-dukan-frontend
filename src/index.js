d996109132c434730a6a3b76f1fd5ade720fad4f
Shivraj <shivraj8404@gmail.com>
2025-05-23 09:40:21 +0530

    App.jsx में routing सही की

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './index.css';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";        // यह आपका पूरा ऐप (Login आदि) है
