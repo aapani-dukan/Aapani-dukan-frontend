55f492530edfe6d5fef4b6fab7877e511a83c309
aapani-dukan <shivraj8404@gmail.com>
2025-05-26 22:11:11 +0530

    Update AuthCallback.js

--- src/pages/AuthCallback.js
+++ src/pages/AuthCallback.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"; import { useNavigate } from "react-router-dom
 
 const AuthCallback = () => { const navigate = useNavigate();
 
-useEffect(() => { const urlParams = new URLSearchParams(window.location.search); const token = urlParams.get("token");
+useEffect(() => { const urlParams = new URLSearchParams(window.location.search); const token = urlParams.get("jwtToken");
 
 if (token) {
 
