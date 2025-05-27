ea6c70e60872fa8a009bd4ef18e4c4e45f4c2f30
aapani-dukan <shivraj8404@gmail.com>
2025-05-27 18:48:41 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-
 import CustomerDashboard from "./pages/CustomerDashboard";
-import SellerDashboard from "./pages/SellerDashboard";
-import SellerRegister from "./pages/SellerRegister";
-import Login from "./pages/Login";
 
 export default function Router() {
   return (
     <Routes>
-      {/* App खुलते ही यह डैशबोर्ड खुलेगा */}
-      <Route path="/customer-dashboard" element={<CustomerDashboard />} />
-
-      {/* बाकी पेज */}
-      <Route path="/home" element={<Home />} />
       <Route path="/" element={<CustomerDashboard />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/seller-register" element={<SellerRegister />} />
-      <Route path="/seller-dashboard" element={<SellerDashboard />} />
-      
     </Routes>
   );
 }
