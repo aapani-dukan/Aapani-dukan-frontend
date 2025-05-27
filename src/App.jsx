351e5a923da43a8fc6b8cff61292aa105b2f4e2f
aapani-dukan <shivraj8404@gmail.com>
2025-05-27 16:10:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Router from "./routes/Router"; // ध्यान दें: path सही रखें
+import Router from "./routes"; // ध्यान दें: path सही रखें
 
 export default function App() {
   return (
