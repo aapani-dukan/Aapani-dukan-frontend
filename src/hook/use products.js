f9d31964720466b132b279e8d4f5485df82ff1c3
aapani-dukan <shivraj8404@gmail.com>
2025-05-27 14:08:08 +0530

    Create use products.js

---
+++ src/hook/use products.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../config";
+
+export default function useProducts() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/products`)
+      .then(res => res.json())
+      .then(data => {
+        const approved = data.filter(p => p.approved);
+        setProducts(approved);
+      })
+      .catch(err => console.error("Product fetch error:", err));
+  }, []);
+
+  return products;
+}
