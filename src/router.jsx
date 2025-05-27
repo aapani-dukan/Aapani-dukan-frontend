9bd44f7aecc1264c65cec2b4d8bc74befcfa1671
aapani-dukan <shivraj8404@gmail.com>
2025-05-27 16:28:10 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import About from "../pages/about";
+
 import CustomerDashboard from "./pages/CustomerDashboard";
 import SellerDashboard from "./pages/SellerDashboard";
 import SellerRegister from "./pages/SellerRegister";
