822f68d9eaae9d3e2201042cdfbdfc2876ed935d
aapani-dukan <shivraj8404@gmail.com>
2025-05-27 14:55:55 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/home";
-import About from "../pages/about";
 import CustomerDashboard from "../pages/CustomerDashboard";
+import About from "../pages/about";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<CustomerDashboard />} />
       <Route path="/about" element={<About />} />
       <Route path="/CustomerDashboard" element={<CustomerDashboard />} />
     </Routes>
