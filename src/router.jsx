58a2b4cf2888ae9a104aea3d6af81eaf1f4c2fe6
aapani-dukan <shivraj8404@gmail.com>
2025-05-28 08:54:27 +0530

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CustomerDashboard from "./pages/CustomerDashboard";
+import AuthCallback from "./pages/AuthCallback"; // अगर आप callback भी चाहते हैं
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<CustomerDashboard />} />
+      <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+      <Route path="/auth-callback" element={<AuthCallback />} />
     </Routes>
   );
 }
